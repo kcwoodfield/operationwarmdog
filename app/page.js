29e0021c7bb6f2364c5482c80b2f5cf1b90f6780
Kevin Woodfield <kcwoodfield@gmail.com>
2024-01-11 11:48:07 -0600

    styles

--- app/page.js
+++ app/page.js
@@ -46,6 +46,11 @@ export default function Home() {
           <h1>Have an idea? Hit us up!</h1>
           <p>Send us a note here: <Link href="mailto:kevin@operationwarmdog.org">kevin@operationwarmdog.org</Link></p>
         </section>
+
+        <section>
+          <h1>Want to drop off warm weather items?</h1>
+          <p>Send us a note here and we'll reply asap: <Link href="mailto:kevin@operationwarmdog.org">kevin@operationwarmdog.org</Link></p>
+        </section>
        </div>
     </main>
   )
