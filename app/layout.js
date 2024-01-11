7ca460ab854bc06b1e47ed90c724266c966acaf9
Kevin Woodfield <kcwoodfield@gmail.com>
2024-01-11 11:21:35 -0600

    cleanup

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Operating Warm Dog',
+  title: 'Operation Warm Dog',
   description: 'Kill the chill in Austin, TX',
 }
 
